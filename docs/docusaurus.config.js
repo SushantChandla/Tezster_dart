ab1b0d205a1f5645fa3632f0fef84fe3d2346d0d
Sushant Chandla <sushantchandla@gmail.com>
2021-09-20 09:44:27 +0530

    url updated

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 (module.exports = {
   title: 'Tezster Dart',
   tagline: 'A flutter package which provides the functionalities to play around with tezos dApps',
-  url: 'https://tezsterdart.netlify.app/',
+  url: 'https://tezsterdart.netlify.app',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
